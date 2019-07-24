8ab468fe2bde75cf85669e699df0dc5e7d27a098
blueginah <blueginah97@gmail.com>
2019-07-24 16:49:40 +0900

    last update

--- a.cc
+++ a.cc
@@ -4,11 +4,9 @@
 int main()
 {
     printf("Hello my name is jiyoung\n");
-<<<<<<< HEAD
     printf("I added printf!!!!!\n\n");
-    return 0;
-=======
     printf("Hello my name is heerock\n");
-	return 0;
->>>>>>> a51a336f27cdda70fe98e2ea5dabca432fb01164
+
+    printf("This is final commit!!!\n\n");
+    return 0;
 }
